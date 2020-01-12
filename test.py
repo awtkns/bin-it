dc5f693b6e1c6b57e846da981a906233aef259de
Ethan <ehaid@sfu.ca>
2020-01-11 18:43:07 -0800

    data CSV

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import io
 import os
+import pandas as pd
 
 # Imports the Google Cloud client library
 from google.cloud import vision
@@ -8,25 +9,32 @@ from google.cloud.vision import types
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.abspath('google-auth.json')
 
 def processImage():
-    # Instantiates a client
-    client = vision.ImageAnnotatorClient()
+    # # Instantiates a client
+    # client = vision.ImageAnnotatorClient()
+    #
+    # # The name of the image file to annotate
+    # file_name = os.path.abspath('example/example01.jpg')
+    #
+    # # Loads the image into memory
+    # with io.open(file_name, 'rb') as image_file:
+    #     content = image_file.read()
+    #
+    # image = types.Image(content=content)
+    #
+    # # Performs label detection on the image file
+    # response = client.label_detection(image=image)
+    # labels = response.label_annotations
+    #
+    # print('Labels:')
+    # for label in labels:
+    #     print(label.description)
 
-    # The name of the image file to annotate
-    file_name = os.path.abspath('example/example01.jpg')
+    # Open recycle data
+    data_file_name = os.path.abspath('recycle-data.csv')
+    data = pd.read_csv(data_file_name)
 
-    # Loads the image into memory
-    with io.open(file_name, 'rb') as image_file:
-        content = image_file.read()
-
-    image = types.Image(content=content)
-
-    # Performs label detection on the image file
-    response = client.label_detection(image=image)
-    labels = response.label_annotations
-
-    print('Labels:')
-    for label in labels:
-        print(label.description)
+    # Process JSON
+    print(data.head())
 
 if __name__ == '__main__':
     processImage()
